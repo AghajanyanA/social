3f1a06b0bb22a9157f28e3afd0c4016138139116
AghajanyanA <astara@null.net>
2022-01-29 23:21:26 +0300

    header reducer optimisation

--- src/redux/headerReducer.js
+++ src/redux/headerReducer.js
@@ -1,12 +1,16 @@
-let init_state = {
-    userName: 'Gagik',
-    userLastName: 'Tsarukyan',
-    avatarURL: 'https://st2.depositphotos.com/1104517/11967/v/950/depositphotos_119675554-stock-illustration-male-avatar-profile-picture-vector.jpg',
+const get_auth_user_avatar = 'get_auth_user_avatar'
+
+const init_state = {
+    avatar: null
 }
 
 
-let headerReducer = (state = init_state, action) => {
+const headerReducer = (state = init_state, action) => {
     switch(action.type) {
+        case get_auth_user_avatar: 
+            return {
+                ...state, avatar: action.url
+            }
         default:
             return {
                 ...state
@@ -14,4 +18,5 @@ let headerReducer = (state = init_state, action) => {
     }
 }
 
+export const getCurrentUserAvatar = url => {return{type: get_auth_user_avatar, url}}
 export default headerReducer
