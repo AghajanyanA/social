548dcec84d072c2a48a787faed5ca7dc497ca933
AghajanyanA <astara@null.net>
2022-01-29 23:17:13 +0300

    CSS Custom properties integration. step 1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
 import React from 'react'
-import Header from './comps/Header/Header';
 import Navbar from './comps/Navbar/Navbar';
 import { BrowserRouter as Router } from 'react-router-dom'
 import RenderRoutes from './routes/Routes';
 import 'antd/dist/antd.css'
+import HeaderContainer from './comps/Header/HeaderContainer';
+
 
 function App() {
+
   return (
     <Router>
       <div className='main'>
-        <Header />
+        <HeaderContainer />
         <Navbar />
-        <div className='content'>
+        <div className='content' content-theme='blue' >
           <RenderRoutes />
         </div>
       </div>
