e0d137ba55741be6cc131fd292e2f7150ecf7fa5
AghajanyanA <astara@null.net>
2022-03-14 15:03:35 +0300

    theme integration

--- src/App.js
+++ src/App.js
@@ -5,21 +5,31 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import RenderRoutes from './routes/Routes';
 import 'antd/dist/antd.css'
 import HeaderContainer from './comps/Header/HeaderContainer';
+import { connect } from 'react-redux';
 
 
 function App() {
-
+  let themeLocalStorage
+  if (!window.localStorage.theme) {
+      themeLocalStorage = 'blue'
+  }
+  else { themeLocalStorage = JSON.parse(window.localStorage.theme) }
   return (
     <Router>
-      <div className='main'>
+      <div className='main' data-theme={themeLocalStorage}>
         <HeaderContainer />
         <Navbar />
-        <div className='content' content-theme='blue' >
+        <div className='content' >
           <RenderRoutes />
         </div>
       </div>
     </Router>
   )
 }
+const mstp = state => {
+  return {
+    theme: state.settings.theme
+  }
+}
 
-export default App;
+export default connect(mstp)(App);
