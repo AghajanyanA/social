478552308d4483a910175b535eaea38a0ba4e9b5
AghajanyanA <astara@null.net>
2022-03-15 21:18:53 +0300

    preloader styling

--- src/comps/assets/loading-gif/Preloader.jsx
+++ src/comps/assets/loading-gif/Preloader.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import preloader1 from './preloader1.svg'
+import c from './preloader.module.css'
 
 const Preloader = (props) => {
     return <>
-        <img src={preloader1} alt=" " />
+        <img src={preloader1} className={c.preloader} alt=" " />
     </>
 }
 
