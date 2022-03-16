e3d4ccea9d1171ea3fdadf70c48dee882639615c
AghajanyanA <astara@null.net>
2022-03-16 23:36:17 +0300

    refactoring & restyling

--- src/comps/assets/buttonComponent/ButtonComponent.jsx
+++ src/comps/assets/buttonComponent/ButtonComponent.jsx
@@ -1,8 +1,9 @@
 import { Button } from "antd";
 import React from "react";
+import c from "./ButtonComponent.module.css"
 
-export const DefaultButtonComponent = ({text, styleName, handler}) => (
-    <Button className={styleName} type="primary" danger onClick={handler}>
+export const DefaultButtonComponent = ({text, styleName = c.defaultBTN, handler, type = "primary", title}) => (
+    <Button className={styleName} type={type} danger onClick={handler} title={title} >
         {text}
     </Button>
 )
